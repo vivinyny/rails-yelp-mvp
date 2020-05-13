fadd330cfa08b7a05f76d431689a7769c95346ce
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-05-13 12:43:40 +0200

    Add review model

---
+++ app/controllers/restautants_controller.rb
@@ -0,0 +1,5 @@
+class RestaurantsController < ApplicationController
+  def method_name
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_101207) do
+ActiveRecord::Schema.define(version: 2020_05_13_104014) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "text"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
